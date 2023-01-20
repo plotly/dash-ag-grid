192d7f2158b2cd555830a6f7a3a20869a57a2de9
AnnMarieW <amward@fastmail.com>
2023-01-20 09:34:57 -0700

    updated cell renderer example to show data updated in a callback

--- docs/examples/rendering/animation_renderer.py
+++ docs/examples/rendering/animation_renderer.py
@@ -1,11 +1,13 @@
+import random
 import dash_ag_grid as dag
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, Input, Output, State
 import pandas as pd
 import numpy as np
 
 app = Dash(__name__)
 
 df = pd.DataFrame(np.random.randint(0, 1000, size=(15, 3)), columns=list("abc"))
+df.reset_index(inplace=True)
 
 columnDefs = [
     {
@@ -46,18 +48,32 @@ defaultColDef = {
 app.layout = html.Div(
     [
         dcc.Markdown(
-            "This grid updates totals and average columns and animates the changed values"
+            "This grid demonstrates the renderer for animating changes to the date.  Try entering new values into the editable columns and press the button to update column c in a callback"
         ),
+        html.Button("Update column C", id="live-data-grid-btn"),
         dag.AgGrid(
             id="live-data-grid",
             columnDefs=columnDefs,
             rowData=df.to_dict("records"),
             columnSize="sizeToFit",
             defaultColDef=defaultColDef,
+            # setting a row ID is required when updating data in a callback
+            setRowId="index",
         ),
     ],
     style={"margin": 20},
 )
+
+
+@app.callback(
+    Output("live-data-grid", "rowData"),
+    Input("live-data-grid-btn", "n_clicks"),
+    State("live-data-grid", "rowData"),
+)
+def update_col_c(_, rows):
+    for r in rows:
+        r["c"] = random.randint(1, 1000)
+    return rows
 
 
 if __name__ == "__main__":
--- docs/pages/rendering/animation_renderer.py
+++ docs/pages/rendering/animation_renderer.py
@@ -8,20 +8,30 @@ register_page(
     order=1,
     description=app_description,
     title="Dash AG Grid Rendering",
-
     # name="Bootstrap Utility Classes",
     # hashtags=["intro","background", "border", "color", "spacing", "text", "position"],
 )
 
 text1 = """
+# Change Cell Renderers
+The grid provides two cell renderers for animating changes to data. They are:
+
+- `agAnimateShowChangeCellRenderer`: The previous value is temporarily shown beside the old value with a directional arrow showing increase or decrease in value. The old value is then faded out.
+- `agAnimateSlideCellRenderer`: The previous value shown in a faded fashion and slides, giving a ghosting effect as the old value fades and slides away.
+
+Example
+The example below shows the `agAnimateShowChangeCellRenderer` To test, try the following:
+
+Columns A,  and B are editable.
+Columns C is updated via clicking the button.
+Changes to any of the first 3 columns results in animations in the Total and Average column.
+
 """
 
 layout = html.Div(
     [
-
         make_md(text1),
         example_app("examples.rendering.animation_renderer", make_layout=make_tabs),
-
         # up_next("text"),
     ],
 )
