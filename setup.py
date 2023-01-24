8a5ce1b461fb29e82d84a2e1d1a312cccaaf463a
alexcjohnson <alex@plot.ly>
2023-01-23 22:42:28 -0500

    setup.py updates

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 import json
-import os
 from setuptools import setup
 
 
-with open('package.json') as f:
+with open("package.json") as f:
     package = json.load(f)
 
 package_name = package["name"].replace(" ", "_").replace("-", "_")
@@ -11,13 +10,23 @@ package_name = package["name"].replace(" ", "_").replace("-", "_")
 setup(
     name=package_name,
     version=package["version"],
-    author=package['author'],
+    author=package["author"],
     packages=[package_name],
     include_package_data=True,
-    license=package['license'],
-    description=package.get('description', package_name),
-    install_requires=[],
+    license=package["license"],
+    description=package.get("description", package_name),
+    install_requires=["dash>=2"],
+    python_requires=">=3.6",
     classifiers = [
-        'Framework :: Dash',
-    ],    
+        "Framework :: Dash",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
 )
