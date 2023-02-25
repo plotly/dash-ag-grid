60c1786fd5a909c7821f7febf3e4f6b9d8180da9
AnnMarieW <amward@fastmail.com>
2023-02-25 15:45:37 -0700

    added cell render custom component examples

---
+++ docs/assets/dashAgGridComponentFunctions.js
@@ -0,0 +1,116 @@
+var dagcomponentfuncs = window.dashAgGridComponentFunctions = window.dashAgGridComponentFunctions || {};
+
+dagcomponentfuncs.stockLink = function (props) {
+    return React.createElement('a',
+    {
+        href: 'https://finance.yahoo.com/quote/' + props.value,
+        target: props.value
+    }, props.value)
+}
+
+dagcomponentfuncs.customDropdown = function (props) {
+
+    function selectionHandler() {
+        const newValue = event.target.value;
+        const colId = props.column.colId;
+        props.node.setDataValue(colId, newValue);
+    }
+
+    const options = props.colDef.cellEditorParams.values.map((opt) => React.createElement('option', {}, opt))
+
+    return React.createElement('div',
+    {style: {'width':'100%','height':'100%', 'padding':'5px', 'display':'flex',
+     'justifyContent':'center', 'alignItems':'center'}},
+     React.createElement('select',
+        {
+            value: props.value,
+            onChange: selectionHandler,
+        }, options
+        )
+    )
+}
+
+dagcomponentfuncs.checkbox = function (props) {
+    function onClick() {
+        if (!('checked' in event.target)) {
+            const checked = !event.target.children[0].checked;
+            const colId = props.column.colId;
+            props.node.setDataValue(colId, checked);
+        }
+    }
+
+    function checkedHandler() {
+        const checked = event.target.checked;
+        const colId = props.column.colId;
+        props.node.setDataValue(colId, checked);
+    }
+    return React.createElement('div',
+    {style: {'width':'100%','height':'100%', 'padding':'5px', 'display':'flex',
+     'justifyContent':'center', 'alignItems':'center'},
+     onClick: onClick},
+     React.createElement('input',
+        {
+            type: 'checkbox',
+            checked: props.value,
+            onChange: checkedHandler,
+            style: {'cursor':'pointer'}
+        })
+    )
+}
+
+dagcomponentfuncs.tags = function (props) {
+
+    const {setProps, data} = props;
+
+    if (props.value == "High") { newTag = React.createElement('div', {
+        style: {"backgroundColor":"#d8f0d3", "borderRadius":"15px", "padding": "5px", "color":"black" },
+        }, props.value)}
+    else if (props.value == "Low") {
+        newTag = React.createElement('div', {
+        style: {"backgroundColor":"#f5cccc", "borderRadius":"15px", "padding": "5px", "color":"black"},
+        }, props.value)
+    }
+    else {
+        newTag = React.createElement('div', {
+        style: {"backgroundColor":"#fffec8", "borderRadius":"15px", "padding": "5px", "color":"black"},
+        }, props.value)
+    }
+
+    return React.createElement('div',
+    {style: {'width':'100%','height':'100%', 'padding':'5px', 'display':'flex',
+     'justifyContent':'center', 'alignItems':'center'}},
+    newTag)
+}
+
+dagcomponentfuncs.myCustomButton = function (props) {
+
+    const {setProps, data} = props;
+
+    if (!props.value) {
+        return React.createElement('button')
+    }
+
+    function onClick() {
+        let colId = props.column.colId;
+        let newData = JSON.parse(JSON.stringify(props.node.data[colId]));
+        newData["n_clicks"]++
+        props.node.setDataValue(colId, newData);
+        props.node.setDataValue('action', colId);
+    }
+
+    const id = JSON.stringify({'index': props.rowIndex, 'type':'customButton'})
+    return React.createElement('div',
+    {style: {'width':'100%','height':'100%', 'padding':'5px', 'display':'flex',
+     'justifyContent':'center', 'alignItems':'center'}},
+    React.createElement('button', {
+        onClick: onClick,
+        id: props.value.id,
+        className: props.value.className,
+
+    }, props.value.children))
+}
+
+dagcomponentfuncs.myCustomTooltip = function (params) {
+    info = [React.createElement('div',{},params.data.ticker), React.createElement('div',{}, params.data.company),React.createElement('div',{}, params.data.price)]
+    return React.createElement('div',{style: {"border":'2pt solid white', 'backgroundColor':'black', 'padding': 10}}, info);
+}
---
+++ docs/examples/rendering/cell_renderer_link.py
@@ -0,0 +1,71 @@
+"""
+styling with custom cell renderer
+"""
+
+import dash_ag_grid as dag
+from dash import Dash, html, dcc
+import pandas as pd
+
+data = {
+    "ticker": ["AAPL", "MSFT", "AMZN", "GOOGL"],
+    "company": ["Apple", "Microsoft", "Amazon", "Alphabet"],
+    "quantity": [75, 40, 100, 50],
+}
+df = pd.DataFrame(data)
+
+columnDefs = [
+    {
+        "headerName": "Stock Ticker",
+        "field": "ticker",
+        # stockLink function is defined in the dashAgGridComponentFunctions.js in assets folder
+        "cellRenderer": "stockLink",
+    },
+    {
+        "headerName": "Company",
+        "field": "company",
+    },
+    {
+        "headerName": "Shares",
+        "field": "quantity",
+        "editable": True,
+    },
+]
+
+
+table = dag.AgGrid(
+    columnDefs=columnDefs,
+    rowData=df.to_dict("records"),
+    columnSize="sizeToFit",
+    defaultColDef={
+        "editable": False,
+    },
+)
+
+
+app = Dash(__name__)
+
+app.layout = html.Div(
+    [dcc.Markdown("Styling rows with cellRenderer"), table],
+    style={"margin": 20},
+)
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
+
+
+"""
+Put the following in the dashAgGridComponentFunctions.js file in the assets folder
+This will register the stockLink function used in the cellRenderer
+
+-----------
+
+var dagcomponentfuncs = window.dashAgGridComponentFunctions = window.dashAgGridComponentFunctions || {};
+
+dagcomponentfuncs.stockLink = function (props) {
+    return React.createElement('a',
+    {
+        href: 'https://finance.yahoo.com/quote/' + props.value,
+        target: props.value
+    }, props.value)
+}
+"""
