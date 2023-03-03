60ed9f249769dce9e29a4c63fda883e83fc49e58
BSd3v <bryan.ri.schroeder@gmail.com>
2023-03-03 16:09:42 -0500

    testing custom `aggFunc`

--- tests/assets/dashAgGridFunctions.js
+++ tests/assets/dashAgGridFunctions.js
@@ -32,3 +32,36 @@ dagfuncs.rowTest = function(params) {
         return 'testing'
     }
 }
+
+dagfuncs.ratioValueGetter = function (params) {
+  if (!(params.node && params.node.group)) {
+    // no need to handle group levels - calculated in the 'ratioAggFunc'
+    return createValueObject(params.data.gold, params.data.silver);
+  }
+}
+dagfuncs.ratioAggFunc = function (params) {
+  let goldSum = 0;
+  let silverSum = 0;
+  params.values.forEach((value) => {
+    if (value && value.gold) {
+      goldSum += value.gold;
+    }
+    if (value && value.silver) {
+      silverSum += value.silver;
+    }
+  });
+  return createValueObject(goldSum, silverSum);
+}
+
+function createValueObject(gold, silver) {
+  return {
+    gold: gold,
+    silver: silver,
+    toString: () => `${gold && silver ? gold / silver : 0}`,
+  };
+}
+
+dagfuncs.ratioFormatter = function (params) {
+  if (!params.value || params.value === 0) return '';
+  return '' + Math.round(params.value * 100) / 100;
+}
