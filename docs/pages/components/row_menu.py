1fdcd17ff2e91c2cd46307afd080d711a93a150c
AnnMarieW <amward@fastmail.com>
2023-03-31 14:42:40 -0700

    a5 - edited components section

--- docs/pages/components/row_menu.py
+++ docs/pages/components/row_menu.py
@@ -12,7 +12,7 @@ register_page(
 )
 
 text1 = """
-# Components
+# Components 
 
 In Dash AG Grid community there are a limited number of in cell components and editors.
 
@@ -25,7 +25,17 @@ Other components:
  - <dccLink href='/components/row-menu' children='Row Menu' />  To access menu options in a callback
  - <dccLink href='/rendering/animation-renderer' children='Cell change animation renderer' />
 
+Custom components:
+
+You can also create custom components and cell renderers.  For examples see:  
+
+ - <dccLink href='/components/cell-renderer' children='Cell Renderers' />  for examples of several custom components`
+ - <dccLink href='/components/overlay' children='Overlay' />  for custom loading and no rows overlay components
+ - <dccLink href='/components/tooltip' children='Tooltip' /> for a custom Tooltip component.
+
 ### Row Menu
+
+This component is included in dash-ag-grid
 
 """
 
