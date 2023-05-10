643dcb8679f1133d85b95a661966e986d3b94312
AnnMarieW <amward@fastmail.com>
2023-05-10 09:05:13 -0700

    Added missing component from DBC_Progress

--- docs/examples/components/cell_renderer_dbc_progress.py
+++ docs/examples/components/cell_renderer_dbc_progress.py
@@ -127,4 +127,31 @@ Put the following in the dashAgGridComponentFunctions.js file in the assets fold
 var dagcomponentfuncs = window.dashAgGridComponentFunctions = window.dashAgGridComponentFunctions || {};
 
 
+// use for making dbc.Progress
+dagcomponentfuncs.DBC_Progress = function (props) {
+    const {setData, data} = props;
+
+    function onClick() {
+        setData(props.value);
+    }
+    return React.createElement(
+        window.dash_bootstrap_components.Progress,
+        {
+            onClick,
+            animated: props.animated,
+            className: props.className,
+            color: props.color,
+            label: (props.label === undefined) ? "": props.value + '%',
+            max: props.max,
+            min: props.min,
+            striped: props.striped,
+            style: props.style,
+            value: props.value
+        },
+    );
+};
+
+
+
+
 """
