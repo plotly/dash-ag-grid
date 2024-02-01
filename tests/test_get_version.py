b44fce531520e1715adeda05e8f2bffedb260ecc
BSd3v <bryan.ri.schroeder@gmail.com>
2024-02-01 16:49:05 -0500

    updating test to not be hard coded

--- tests/test_get_version.py
+++ tests/test_get_version.py
@@ -1,4 +1,4 @@
 import dash_ag_grid
 
 def test_gv001_grid_version():
-    assert dash_ag_grid.grid_version == '31.0.3'
+    assert dash_ag_grid.grid_version == dash_ag_grid.package['dependencies']['ag-grid-community']
