eac3655380b332491ebe6200e0802820d367532c
BSd3v <bryan.ri.schroeder@gmail.com>
2024-02-01 13:28:25 -0500

    updating the `test_version` to be more robust and not break at the first error, allowing it to check all 3 grid dependencies.

--- test_version.py
+++ test_version.py
@@ -1,16 +1,19 @@
-import os as _os, json
+import os as _os, json, re
 
 _basepath = _os.path.dirname(__file__)
 _filepath = _os.path.abspath(_os.path.join(_basepath, 'package.json'))
 with open(_filepath) as f:
     package = json.load(f)
 
-grid_version = package['dependencies']['ag-grid-community']
-# grid version must be a valid locked version of the grid, to limit possible unexpected updates and breaking changes
 
-try:
-    if int(grid_version[0]) > 0:
-        pass
-except:
-    print("not a valid version of AG-Grid; grid version must be a valid locked version of the grid, to limit possible unexpected updates and breaking changes")
-    raise
+# grid version must be a valid locked version of the grid, to limit possible unexpected updates and breaking changes
+errors = []
+for pk in ['ag-grid-community', 'ag-grid-enterprise', 'ag-grid-react']:
+    try:
+        assert re.match("^\d+[.]\d+[.]\d+$", package['dependencies'][pk])
+    except:
+        errors.append(f"{pk} - {package['dependencies'][pk]}")
+if errors:
+    message = "not a valid version of AG-Grid; grid version must be a valid locked version of the grid," \
+              " to limit possible unexpected updates and breaking changes\n"
+    raise Exception(message + '\n'.join(errors))
