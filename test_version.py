0dae37dd13592c07c7c29a892b9c612d5be6db82
BSd3v <bryan.ri.schroeder@gmail.com>
2024-02-01 13:37:43 -0500

    adjusting `test_version.py` to test the versions of `AG Grid` are the same across the dependencies. This error will point to needing to disable the test if AG Grid becomes out of sync across the versions.

--- test_version.py
+++ test_version.py
@@ -8,8 +8,14 @@ with open(_filepath) as f:
 
 # grid version must be a valid locked version of the grid, to limit possible unexpected updates and breaking changes
 errors = []
+prev_version = ''
 for pk in ['ag-grid-community', 'ag-grid-enterprise', 'ag-grid-react']:
     try:
+        if prev_version:
+            if prev_version != package['dependencies'][pk]:
+                errors.append(f"{pk} - {package['dependencies'][pk]} doesnt equal other version of {prev_version},"
+                              f" if AG Grid becomes out of sync with itself, disable this line: `test_version.py:16-17`")
+        prev_version = package['dependencies'][pk]
         assert re.match("^\d+[.]\d+[.]\d+$", package['dependencies'][pk])
     except:
         errors.append(f"{pk} - {package['dependencies'][pk]}")
