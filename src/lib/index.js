a32e47fdaca99651e3cc6704b5ac96b82cca4886
Godisemo <847898+Godisemo@users.noreply.github.com>
2025-02-06 01:02:56 +0100

    Remove theme export

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,4 +2,3 @@ import AgGrid, {getApi, getApiAsync} from './components/AgGrid.react';
 
 export {AgGrid, getApi, getApiAsync};
 export {useGridFilter} from 'ag-grid-react';
-export {createTheme, themeQuartz, themeBalham, themeBalham, } from 'ag-grid-community'
