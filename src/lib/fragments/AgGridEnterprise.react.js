b2b9f55ae0c6defde9517b8602f643993829a707
Adrian Borrmann <hello@adrianborrmann.com>
2025-07-10 16:40:18 -0600

    code cleanup

--- src/lib/fragments/AgGridEnterprise.react.js
+++ src/lib/fragments/AgGridEnterprise.react.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import {ModuleRegistry} from 'ag-grid-community';
-import {AllEnterpriseModule, LicenseManager, SparklinesModule} from 'ag-grid-enterprise';
-import { AgChartsEnterpriseModule } from 'ag-charts-enterprise';
+import {
+    AllEnterpriseModule,
+    LicenseManager,
+    SparklinesModule,
+} from 'ag-grid-enterprise';
+import {AgChartsEnterpriseModule} from 'ag-charts-enterprise';
 import MemoizedAgGrid, {propTypes} from './AgGrid.react';
 
 // Register all enterprise features
-ModuleRegistry.registerModules([AllEnterpriseModule, SparklinesModule.with(AgChartsEnterpriseModule)]);
-
+ModuleRegistry.registerModules([
+    AllEnterpriseModule,
+    SparklinesModule.with(AgChartsEnterpriseModule),
+]);
 
 export default function DashAgGridEnterprise(props) {
     const {licenseKey} = props;
