cdfff73f9a685132603f611e614d9aa0f7b0961c
Adrian Borrmann <hello@adrianborrmann.com>
2025-07-23 14:06:58 -0600

    Add e2e test for legacy theme css

--- tests/test_user_style.py
+++ tests/test_user_style.py
@@ -2,6 +2,7 @@ from dash import Dash, html, Output, Input, no_update, State, ctx
 import dash_ag_grid as dag
 import plotly.express as px
 import json
+import pytest
 
 from . import utils
 from dash.testing.wait import until
@@ -61,3 +62,58 @@ def test_us001_user_style(dash_duo):
         in dash_duo.find_element("div.ag-theme-alpine").get_attribute("style"),
         timeout=3,
     )
+
+
+@pytest.mark.parametrize("theme", ["alpine", "balham", "material", "quartz"])
+def test_us002_legacy_themes(dash_duo, theme):
+    app = Dash(
+        __name__,
+        external_stylesheets=[
+            dag.themes.BASE,
+            dag.themes.ALPINE,
+            dag.themes.BALHAM,
+            dag.themes.MATERIAL,
+            dag.themes.QUARTZ,
+        ],
+    )
+
+    columnDefs = [
+        {"field": "name", "width": "500"},
+    ]
+
+    rowData = [
+        {"name": "a"},
+        {"name": "b"},
+        {"name": "c"},
+    ]
+
+    app.layout = html.Div(
+        [
+            dag.AgGrid(
+                id="grid",
+                columnDefs=columnDefs,
+                rowData=rowData,
+                dashGridOptions={"theme": "legacy"},
+                className=f"ag-theme-{theme}",
+            ),
+        ]
+    )
+
+    dash_duo.start_server(app)
+
+    grid = utils.Grid(dash_duo, "grid")
+
+    grid.wait_for_cell_text(0, 0, "a")
+
+    # Test that the CSS files are actually loaded and applied
+
+    # Base styles: assert that the grid height is <= 400px because an unstyled
+    # grid is very "tall"
+    root_wrapper = dash_duo.find_element(".ag-root-wrapper")
+    wrapper_height = root_wrapper.size["height"]
+    assert wrapper_height <= 400, f"Grid appears to be unstyled: height is too tall ({wrapper_height}px)"
+
+    # Specific themes: Assert that cell headers are bold
+    header_cell_text = dash_duo.find_element(".ag-header-cell-text")
+    font_weight = header_cell_text.value_of_css_property("font-weight")
+    assert font_weight in ["bold", "700", "600", "500",], "Grid appears to be unstyled: cell headers are not bold"
