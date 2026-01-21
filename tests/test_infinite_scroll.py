261fbbdd90da300e0f67e4c6f898612d00c8ac80
BSd3v <82055130+BSd3v@users.noreply.github.com>
2026-01-21 13:27:19 -0500

    adjusting the expected cells and values

--- tests/test_infinite_scroll.py
+++ tests/test_infinite_scroll.py
@@ -149,15 +149,12 @@ def test_is001_infinite_scroll(dash_duo):
     ## testing sorting with responses
     grid.get_header_cell(0).click()
     grid.wait_for_cell_text(0, 0, "0")
-    time.sleep(.2)
     grid.get_header_cell(0).click()
     grid.wait_for_cell_text(0, 0, "9999")
-    time.sleep(.2)
     grid.get_header_cell(1).click()
-    grid.wait_for_cell_text(1, 0, "3600")
-    time.sleep(.2)
+    grid.wait_for_cell_text(0, 1, "0-0-0")
     grid.get_header_cell(1).click()
-    grid.wait_for_cell_text(0, 0, "7263")
+    grid.wait_for_cell_text(0, 1, "4-6-7")
 
 
 def test_is002_infinite_scroll_styling(dash_duo):
